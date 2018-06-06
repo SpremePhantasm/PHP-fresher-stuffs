5400ff2a27342aea5620727f9be7625c79179fde
SpremePhantasm <34367798+SpremePhantasm@users.noreply.github.com>
2018-06-06 09:58:16 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,22 @@
+﻿<?php
+/*check số chẵn*/
+class testEven {
+	function isEven($a) {
+		if (($a%2)==0) {return true;}
+		else {return false;}
+	}
+};
+$testEven = new testEven;
+return $testEven->isEven(314);
+
+/*check số nguyên tố*/
+class testPrime {
+	function isPrime($b) {
+		for ($x=3;$x<($b/2);$x++) {
+			if (($b%$x)==0) {return false;}
+			else {return true;}
+		};
+	}
+};
+$testPrime = new testPrime;
+return $testPrime->isPrime(87);
