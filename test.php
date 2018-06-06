533a03ae8aa396d6b4abfd4eabb2ac2ab98d850d
SpremePhantasm <34367798+SpremePhantasm@users.noreply.github.com>
2018-06-06 10:46:03 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -2,21 +2,20 @@
 /*check số chẵn*/
 class testEven {
 	function isEven($a) {
-		if (($a%2)==0) {echo "là số chẵn";}
-		else {echo "là số lẻ";}
+		if (($a%2)==0) {echo $a." là số chẵn";}
+		else {echo $a." là số lẻ";}
 	}
 };
 $testEven = new testEven;
 echo $testEven->isEven(314);
-
 /*check số nguyên tố*/
 class testPrime {
 	function isPrime($b) {
 		for ($x=3;$x<($b/2);$x++) {
-			if (($b%$x)==0) {echo "không phải số nguyên tố"; break;}
-			else {echo "là số nguyên tố";}
+			if (($b%$x)==0) {echo $b." không phải là số nguyên tố"; break;}
 		};
+		echo $b." là số nguyên tố";
 	}
 };
 $testPrime = new testPrime;
-echo $testPrime->isPrime(66);
+echo $testPrime->isPrime(2);
