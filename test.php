3c26f44d22f3a2c9c60680e23027a0b7b7f5e04c
SpremePhantasm <34367798+SpremePhantasm@users.noreply.github.com>
2018-06-07 17:32:11 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -34,14 +34,15 @@ class testArray {
 				break;
 			
 			}
+		}; 
 			if ($flag == true) {
 				echo "<br>";echo "Tổng các số của array là ".array_sum($array);
 				echo "<br>";echo "Số lớn nhất của array là ".max($array);
-				break;
+				
 				
 			}
-		}; 
+		
 	}
 };
  $testArray = new testArray;
- echo $testArray->sumArray(array(2,4,6.675,75,41,869,4));
+ echo $testArray->sumArray(array(2,4,6,75,41,869,4));
